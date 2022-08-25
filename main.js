63aac8ff9f9640116eb48747978feaff53623688
Daniel Vera <d.vera.acero@gmail.com>
2022-08-25 16:43:45 -0500

    Se añadio media queries para estilizar menu en dispositivos

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 
+const menuMobileIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
 menuEmail.addEventListener('click', toogleDesktopMenu)
+menuMobileIcon.addEventListener('click', tooglemobileMenu)
 
 function toogleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')    
+}
+
+function tooglemobileMenu() {
+    mobileMenu.classList.toggle('inactive')    
 }
