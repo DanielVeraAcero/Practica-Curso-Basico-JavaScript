7bbd581229c6271ff43dc7ec109ff4c4d1ca1637
Daniel Vera <d.vera.acero@gmail.com>
2022-08-30 17:50:51 -0500

    Se mejoro display de menu auxiliares con condicionales para la version mobile

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const carIcon = document.querySelector('.navbar-shopping-cart')
 
 const menuMobileIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toogleDesktopMenu)
 menuMobileIcon.addEventListener('click', tooglemobileMenu)
+carIcon.addEventListener('click', toogleaside)
 
 function toogleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')    
 }
 
 function tooglemobileMenu() {
+    const asideClosed = aside.classList.contains('inactive');
+
+    if (!asideClosed) {
+        aside.classList.add('inactive')
+    }
+
     mobileMenu.classList.toggle('inactive')    
+}
+
+function toogleaside() {
+    const mobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!mobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive')
 }
